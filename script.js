bab1d2a591da64dca6104816c41adc2cccff10e1
João Marcelo Costa de Santana <159437965+jm-236@users.noreply.github.com>
2024-02-29 19:55:52 -0300

    criação do código completo

---
+++ script.js
@@ -0,0 +1,45 @@
+// 1- Pegar a data atual
+var dataAtual = new Date()
+
+// 2- A partir da data atual, calcular a data do próximo aniversario
+anoNiver = dataAtual.getFullYear()
+
+if (dataAtual.getMonth() == 5 && dataAtual.getDate() == 23) {
+    anoNiver = anoNiver++
+}
+else if (dataAtual.getMonth() > 5) {
+    anoNiver = anoNiver++
+}
+var niver = new Date(anoNiver,5,23,0,0,0,0)
+
+// 3- Calculo a diferença das duas e ponho nos textos
+var tempoRestante = (niver - dataAtual) // tempo restante em ms
+var segundosRestantes = (tempoRestante - (tempoRestante % 1000)) / 1000 // tempo restante em segundos
+
+// calculo dos minutos restantes
+var minutosRestantes = (segundosRestantes - (segundosRestantes % 60)) / 60
+var segundosRestantes = segundosRestantes % 60
+
+// calculo das horas
+var horasRestantes = (minutosRestantes - (minutosRestantes % 60)) / 60
+var minutosRestantes = minutosRestantes % 60
+
+// calculo dos dias
+var diasRestantes = (horasRestantes - (horasRestantes % 24)) / 24
+var horasRestantes = horasRestantes % 24
+
+// calculo dos meses
+var mesesRestantes = (diasRestantes- (diasRestantes % 30)) / 30
+var diasRestantes = diasRestantes % 30
+
+// exibindo os valores na tela
+document.getElementById("meses").innerHTML = mesesRestantes
+document.getElementById("dias").innerHTML = diasRestantes
+document.getElementById("horas").innerHTML = horasRestantes
+document.getElementById("minutos").innerHTML = minutosRestantes
+document.getElementById("segundos").innerHTML = segundosRestantes
+// obs.: fazer isso a todo momento, como se fosse um cronometro mesmo
+setTimeout(function() {
+    location.reload();
+  }, 1000);
+// essa funcao basicamente vai calcular a diferenca temporal entre uma data e outra
